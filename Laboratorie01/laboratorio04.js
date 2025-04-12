554eb432f6dd26b8d3435c065bd16b5682492012
Raúl Ferreyra <raulrferreyrav@gmail.com>
2025-04-12 00:55:59 -0500

    Added info for JS

---
+++ Laboratorie01/laboratorio04.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+app.use(express.static(__dirname + '/public/laboratorio04'));
